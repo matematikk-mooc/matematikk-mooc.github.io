186265c18487166f8bda4187ea016e0775c1b37a
Kjell-Magne Øierud <kjellm@oierud.net>
2015-02-12 14:45:03 +0100

    Update frontend

--- frontend/mmooc-min.js
+++ frontend/mmooc-min.js
@@ -1201,7 +1201,7 @@ $(document).ready(function() {
     });
 
     mmooc.routes.addRouteForPath(/\/login$/, function() {
-        $('#register-link').innerHTML("<i>Trenger du en konto?</i><b>Klikk her.</b>");
+        $('#register-link').html("<i>Trenger du en konto?</i><b>Klikk her.</b>");
     });
 
     mmooc.routes.addRouteForPath(/\/courses$/, function() {
